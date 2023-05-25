b3e9a6b7a9086cb070d48326bf6ce9af0d115187
NVTHANH229 <nvthanh229@gmail.com>
2023-05-25 13:53:02 +0700

    Add rails admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 require 'sidekiq/web'
 
 Rails.application.routes.draw do
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   resources :order_items
   resources :orders
   resources :products
