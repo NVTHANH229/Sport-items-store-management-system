bca5eae0bb9b461f41ef65f09a8b0044ca131efe
NVTHANH229 <nvthanh229@gmail.com>
2023-05-25 14:31:08 +0700

    Config for Railway deployment

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,6 @@ module Demo9
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.hosts << "nhom13-sport-items-store-management-system.up.railway.app"
   end
 end
